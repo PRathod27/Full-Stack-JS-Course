1af8b0793ddb9deef433e1818d1695aad30d6266
Prem Rathod <prem.rathod.2715@gmail.com>
2025-03-03 22:54:38 +0530

    Added switch statement

--- 2. Control Flow/app.js
+++ 2. Control Flow/app.js
@@ -1,31 +1,61 @@
 // Type Conversion
+
 // 1. String to number
-let money = "50";
+const money = "50";
 // parseInt() - converts string to integer
-console.log(typeof parseInt(money));
+console.log(typeof parseInt(money)); // Output: number
 
 // 2. Number to string
-let num = 50;
+const num = 50;
 // toString() - converts number to string
-console.log(typeof num.toString());
+console.log(typeof num.toString()); // Output: string
 
-// String to float
-let floatnum = "50.44";
+// 3. String to float
+const floatnum = "50.44";
 // parseFloat() - converts string to float
-console.log(typeof parseFloat(floatnum));
+console.log(typeof parseFloat(floatnum)); // Output: number
 
 // Conditional Statements
 
 // 1. if else
 // Syntax
 // if (condition) {
-//   code block will execute if condition is true
+//   code block to be executed
+// } else if (condition) {
+//   code block to be executed
 // } else {
-//   code block will execute if condition is false
+//   code block to be executed
 // }
-let age = 21;
-if (age >= 18) {
+
+const age = 21;
+if (age > 18) {
   console.log("You are eligible to vote");
+} else if (age >= 18 && age <= 21) {
+  console.log("You can still vote");
 } else {
-  console.log("You are not eligible to vote");
+  console.log("You are not eligible for voting");
 }
+
+// switch (expression) {
+// case x:
+// print
+// break;
+// case y:
+// print
+// break;
+// }
+
+let x = 0;
+let text;
+switch (x) {
+  case 0:
+    text = "offf";
+    break;
+  case 1:
+    text = "On";
+    break;
+  default:
+    text = "Nothing";
+}
+
+console.log(text);
