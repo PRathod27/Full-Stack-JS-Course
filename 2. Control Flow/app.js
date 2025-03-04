f83e097726d22d67e62691702b70c484928e9a8c
Prem Rathod <prem.rathod.2715@gmail.com>
2025-03-04 10:54:43 +0530

    For loop and Do while added

--- 2. Control Flow/app.js
+++ 2. Control Flow/app.js
@@ -27,14 +27,14 @@ console.log(typeof parseFloat(floatnum)); // Output: number
 //   code block to be executed
 // }
 
-const age = 21;
-if (age > 18) {
-  console.log("You are eligible to vote");
-} else if (age >= 18 && age <= 21) {
-  console.log("You can still vote");
-} else {
-  console.log("You are not eligible for voting");
-}
+// const age = 21;
+// if (age > 18) {
+//   console.log("You are eligible to vote");
+// } else if (age >= 18 && age <= 21) {
+//   console.log("You can still vote");
+// } else {
+//   console.log("You are not eligible for voting");
+// }
 
 // switch (expression) {
 // case x:
@@ -45,17 +45,50 @@ if (age > 18) {
 // break;
 // }
 
-let x = 0;
-let text;
-switch (x) {
-  case 0:
-    text = "offf";
-    break;
-  case 1:
-    text = "On";
-    break;
-  default:
-    text = "Nothing";
+// let x = 0;
+// let bulb;
+// switch (x) {
+//   case 0:
+//     bulb = "offf";
+//     break;
+//   case 1:
+//     bulb = "On";
+//     break;
+//   default:
+//     bulb = "Nothing";
+// }
+// console.log(bulb);
+
+// let fruit = "Orange";
+
+// switch (fruit) {
+//   case "Banana":
+//     console.log("Banana is yellow");
+//     break;
+//   case "Orange":
+//     console.log("Orange is orange");
+//     break;
+//   case "Apple":
+//     console.log("Apple is red");
+//     break;
+//   default:
+//     console.log("I have no idea what fruit is this");
+// }
+
+// Loops
+
+// for loop syntax
+// for (intitalExpression; condition; incrementExpression) {
+// code block to be executed
+// }
+let i = 0;
+for (let i = 0; i < 5; i++) {
+  console.log("Hello World");
 }
 
-console.log(text);
+do {
+  console.log("Do loop executed");
+} while (i < 5);
+{
+  console.log("While loop executed");
+}
