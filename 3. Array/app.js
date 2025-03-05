645219f6af5429c8d60741e3f50dbfd324bdbf66
Prem Rathod <prem.rathod.2715@gmail.com>
2025-03-05 13:31:10 +0530

    Array topic addded

---
+++ 3. Array/app.js
@@ -0,0 +1,12 @@
+const array = ["one", "two", "three", "four", "five"];
+
+console.log(array[0]);
+console.log(array[1]);
+
+// Nested array
+const nestaaray = ["one", ["two", "three"], 1, true, false];
+console.log(nestaaray);
+console.log(nestaaray[2]);
+
+const newnestarray = ["one", { id: 3 }, true];
+console.log([newnestarray[1]["id"]]);
