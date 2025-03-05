6d07d5cd983785b3691f4178572d42536cf51199
Prem Rathod <prem.rathod.2715@gmail.com>
2025-03-05 13:43:56 +0530

    Array Techniques added.

---
+++ 4. Array Techniques/app.js
@@ -0,0 +1,31 @@
+const fruits = [
+  "apple",
+  "banana",
+  "cherry",
+  "date",
+  "elderberry",
+  "fig",
+  "grape",
+];
+
+// 1. Using the Push() method
+fruits.push("watermelon");
+console.log(fruits);
+
+// 2. Using the Pop() method
+fruits.pop();
+console.log(fruits);
+
+// 3. Using the Shift() method
+fruits.shift();
+console.log(fruits);
+
+// 4. Using the Unshift() method
+fruits.unshift("apple");
+console.log(fruits);
+
+// 5. Using the concat() method
+
+const moreFruits = ["kiwi", "lemon", "mango"];
+const allFruits = fruits.concat(moreFruits);
+console.log(allFruits);
