f172f842780ff96e62d7e391f7ff11983058c1ed
Prem Rathod <prem.rathod.2715@gmail.com>
2025-03-23 20:04:59 +0530

    started with DOM

---
+++ 18. What is Dom/app.js
@@ -0,0 +1,5 @@
+console.dir(document);
+console.log(document.head);
+console.log(document.title);
+console.log(document.fullscreenEnabled);
+console.log(document.URL);
---
+++ 19. Accessing Dom Elements/app.js
@@ -0,0 +1,7 @@
+console.log(document.getElementsByTagName("h1"));
+console.log(document.getElementById("main"));
+console.log(document.getElementsByClassName("cls"));
+console.log(document.querySelector("ul"));
+console.log(document.querySelector("#main"));
+console.log(document.querySelector("#lst"));
+console.log(document.querySelectorAll("li"));
